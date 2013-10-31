a66391f5e125d274c2601189a1b530fdf11c7fae
Daryl Hornsby <digitaldarylh@gmail.com>
2013-10-31 11:56:36 +0100

    New files

---
+++ Assets/Scripts/MouseFire.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseFire : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Shoot the ray on mouse click
+	    if (Input.GetButtonUp("Fire1"))
+	    {
+	        RaycastHit hit;
+	 
+	        // Cast a ray forward from our position for the specified distance
+	        if (Physics.Raycast(transform.position, transform.forward, hit, distance))
+	        {
+				
+			}	    
+	    }
+	}
+}
