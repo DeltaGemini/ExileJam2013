0fdd635d5f1ffc84c52b8d64607ba05435d270e8
Daryl Hornsby <digitaldarylh@gmail.com>
2013-11-02 12:22:10 +0100

    Cleanup

---
+++ Assets/Scripts/EnemyAnimationCue.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAnimationCue : MonoBehaviour {
+	
+	public GameObject parent;
+	
+	// Use this for initialization
+	void RoarSound(){
+		parent.SendMessage("PlaySound");
+	}
+}
